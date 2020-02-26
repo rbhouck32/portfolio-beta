b9432acc78e19e9b16d965e59b59db4d47c7542b
Robert Houck <57123769+rbhouck32@users.noreply.github.com>
2020-02-25 19:03:48 -0500

    This commit is the includes several changes such as the refactoring of the architecture of the site. Trying to break down styling by components. Also the Addition of the Projects Page

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Anchor, NavWrap } from "../Style";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <NavWrap>
-      <Anchor href="#">About</Anchor>
+      <Link to="/about">
+        <Anchor>About</Anchor>
+      </Link>
       <Anchor href="#">Skills</Anchor>
       <Anchor href="#">Projects</Anchor>
     </NavWrap>
